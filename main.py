534e77704ae8d9ed3b7f7bf15d690c22ca542762
DouglasChan <douglastschanto@gmail.com>
2020-06-16 16:05:17 -0400

    This is commit #1c. Moved network code into its own thing. Will have to merge code for multiple levels soon.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import twitter_mention_frequency
 import twitter_get_user_timeline
 import NLP_stats_repackaged
 import Doc_to_Vec_Refactored
+import network_analysis
 #Test
 from nltk import FreqDist
 import sys
@@ -10,10 +11,6 @@ from subprocess import call
 import os.path
 from os import path
 import time
-
-import networkx as nx
-from nltk.tokenize import word_tokenize
-import matplotlib.pyplot as plt
 
 #Assuming you're starting with a profile that actually exists...
 
@@ -89,126 +86,7 @@ def nlp_similarity(placeholder_input):
 
     json_filenames, doc_model = Doc_to_Vec_Refactored.doc_similarity(placeholder_input, word_filter_all)
     return json_filenames, doc_model
-    
-def network_stuff(files, model):
 
-    network_dict = {}
-    
-    for tweet_document in json_filenames:
-        most_similar = model.docvecs.most_similar(tweet_document)[0][0]
-    
-        for tweet_document_compare in json_filenames:
-            similarity = model.docvecs.similarity(tweet_document,tweet_document_compare)
-            print("This is the similarity for %s, and %s. It is %s." %(tweet_document, tweet_document_compare, similarity))
-            
-            #network_dict.setdefault((tweet_document, tweet_document_compare), []).append(similarity) #Network
-            tweet_1_name = tweet_document[14:]
-            tweet_1_name = tweet_1_name[:-6]
-            tweet_2_name = tweet_document_compare[14:]
-            tweet_2_name = tweet_2_name[:-6]
-            network_dict.setdefault((tweet_1_name, tweet_2_name), similarity)
-        
-        print('----------')    
-
-    G = nx.Graph()
-    _ = [G.add_edge(i[0], i[1], weight = j) for i,j in network_dict.items() if j > 0.44]; #j[0]? ; #0.425 for 50 ; #0.45 for 100
-
-    print('Booga')
-    time.sleep(10)
-    
-    print(len(G))
-    print(len(G.edges))
-    print(G.edges)
-    print(type(G.edges))
-    
-    #Removing self ones?
-    edge_list = list(G.edges)
-    edge_removal_list = []
-    for i in range(len(edge_list)):
-        print(edge_list[i])
-        
-        if edge_list[i][0] == edge_list[i][1]:
-            edge_removal_list.append(edge_list[i])
-            
-    print(edge_removal_list)
-    
-    
-    print('Tooktook')
-    time.sleep(12)
-    
-    
-    for i in range(len(edge_removal_list)):
-        G.remove_edge(edge_removal_list[i][0],edge_removal_list[i][1])
-        
-    G.remove_nodes_from(list(nx.isolates(G)))
-    isolate_list = list(nx.isolates(G))
-    
-    
-    
-    #Clustering portion
-    from networkx.algorithms.community import greedy_modularity_communities
-
-    clusters = greedy_modularity_communities(G)
-    
-    print(f'The social network has {len(clusters)} clusters.')
-    
-    plt.figure(figsize=(13,7))
-    #nx.draw_kamada_kawai(G, with_labels = True)
-    
-    
-    def set_cluster_number(G, cluster_list):
-        for i, cluster in enumerate(cluster_list):
-            for node in cluster:
-                G.nodes[node]['cluster'] = i+1
-                
-    misc_clusters = [13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50]
-            
-    def get_node_color(node):
-
-        if node['cluster'] == 1:
-            color = 'pink'
-        elif node['cluster'] == 2:
-            color = 'lightblue'
-        elif node['cluster'] == 3:
-            color = 'lightgreen'
-        elif node['cluster'] == 4:
-            color = 'blue'
-        elif node['cluster'] == 5:
-            color = 'red'
-        elif node['cluster'] == 6:
-            color = 'orange'
-        elif node['cluster'] == 7:
-            color = 'purple'
-        elif node['cluster'] == 8:
-            color = 'brown'
-        elif node['cluster'] == 9:
-            color = 'darkblue'
-        elif node['cluster'] == 10:
-            color = 'green'
-        elif node['cluster'] == 11 :
-            color = 'darkgreen'
-        elif node['cluster'] == 12:
-            color = 'lightred'
-        elif node['cluster'] in misc_clusters:
-            color = 'grey'
-            
-        else:   
-            pass
-        return color
-        
-    set_cluster_number(G, clusters)
-    
-    options = {'with_labels': True, 
-               'node_color':[get_node_color(G.nodes[n]) for n in G.nodes],
-               'edge_color':'grey',
-               'font_size': 10}
-    positions = nx.kamada_kawai_layout(G)
-
-    plt.figure(figsize=(20,10))
-    nx.draw(G, positions, **options)
-    
-    
-    plt.savefig( "g.png" )
 
 
 if __name__ == '__main__':
@@ -223,4 +101,4 @@ if __name__ == '__main__':
                
     json_filenames, doc_model = nlp_similarity(f_name_list)        
      
-    network_stuff(json_filenames, doc_model)
+    network_analysis.network_stuff(json_filenames, doc_model)
---
+++ network_analysis.py
@@ -0,0 +1,124 @@
+import networkx as nx
+from nltk.tokenize import word_tokenize
+import matplotlib.pyplot as plt
+import time
+
+def network_stuff(files, model):
+
+    network_dict = {}
+    
+    for tweet_document in files:
+        most_similar = model.docvecs.most_similar(tweet_document)[0][0]
+    
+        for tweet_document_compare in files:
+            similarity = model.docvecs.similarity(tweet_document,tweet_document_compare)
+            print("This is the similarity for %s, and %s. It is %s." %(tweet_document, tweet_document_compare, similarity))
+            
+            #network_dict.setdefault((tweet_document, tweet_document_compare), []).append(similarity) #Network
+            tweet_1_name = tweet_document[14:]
+            tweet_1_name = tweet_1_name[:-6]
+            tweet_2_name = tweet_document_compare[14:]
+            tweet_2_name = tweet_2_name[:-6]
+            network_dict.setdefault((tweet_1_name, tweet_2_name), similarity)
+        
+        print('----------')    
+
+    G = nx.Graph()
+    _ = [G.add_edge(i[0], i[1], weight = j) for i,j in network_dict.items() if j > 0.44]; #j[0]? ; #0.425 for 50 ; #0.45 for 100
+
+    print('Booga')
+    time.sleep(10)
+    
+    print(len(G))
+    print(len(G.edges))
+    print(G.edges)
+    print(type(G.edges))
+    
+    #Removing self ones?
+    edge_list = list(G.edges)
+    edge_removal_list = []
+    for i in range(len(edge_list)):
+        print(edge_list[i])
+        
+        if edge_list[i][0] == edge_list[i][1]:
+            edge_removal_list.append(edge_list[i])
+            
+    print(edge_removal_list)
+    
+    
+    print('Tooktook')
+    time.sleep(12)
+    
+    
+    for i in range(len(edge_removal_list)):
+        G.remove_edge(edge_removal_list[i][0],edge_removal_list[i][1])
+        
+    G.remove_nodes_from(list(nx.isolates(G)))
+    isolate_list = list(nx.isolates(G))
+    
+    
+    
+    #Clustering portion
+    from networkx.algorithms.community import greedy_modularity_communities
+
+    clusters = greedy_modularity_communities(G)
+    
+    print(f'The social network has {len(clusters)} clusters.')
+    
+    plt.figure(figsize=(13,7))
+    #nx.draw_kamada_kawai(G, with_labels = True)
+    
+    
+    def set_cluster_number(G, cluster_list):
+        for i, cluster in enumerate(cluster_list):
+            for node in cluster:
+                G.nodes[node]['cluster'] = i+1
+                
+    misc_clusters = [13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50]
+            
+    def get_node_color(node):
+
+        if node['cluster'] == 1:
+            color = 'pink'
+        elif node['cluster'] == 2:
+            color = 'lightblue'
+        elif node['cluster'] == 3:
+            color = 'lightgreen'
+        elif node['cluster'] == 4:
+            color = 'blue'
+        elif node['cluster'] == 5:
+            color = 'red'
+        elif node['cluster'] == 6:
+            color = 'orange'
+        elif node['cluster'] == 7:
+            color = 'purple'
+        elif node['cluster'] == 8:
+            color = 'brown'
+        elif node['cluster'] == 9:
+            color = 'darkblue'
+        elif node['cluster'] == 10:
+            color = 'green'
+        elif node['cluster'] == 11 :
+            color = 'darkgreen'
+        elif node['cluster'] == 12:
+            color = 'lightred'
+        elif node['cluster'] in misc_clusters:
+            color = 'grey'
+            
+        else:   
+            pass
+        return color
+        
+    set_cluster_number(G, clusters)
+    
+    options = {'with_labels': True, 
+               'node_color':[get_node_color(G.nodes[n]) for n in G.nodes],
+               'edge_color':'grey',
+               'font_size': 10}
+    positions = nx.kamada_kawai_layout(G)
+
+    plt.figure(figsize=(20,10))
+    nx.draw(G, positions, **options)
+    
+    
+    plt.savefig( "g.png" )
