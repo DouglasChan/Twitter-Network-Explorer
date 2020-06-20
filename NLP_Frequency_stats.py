5e9038e97af9ea877c60742b813d62c987b7f89f
DouglasChan <douglastschanto@gmail.com>
2020-06-20 14:53:02 -0400

    This is commit #1w. Still need to rewrite since not doing frequency distribution on the right level.

--- NLP_Frequency_stats.py
+++ NLP_Frequency_stats.py
@@ -22,20 +22,29 @@ def content_filter(text):
     return content
 
 def frequency_analysis(cluster_setlist): #Take from network_analysis
+    
+    frequency_unigram_stats = []
+    frequency_bigram_stats = []
  
     #Do for every json file in the cluster next*
     
     for cluster_list in range(len(cluster_setlist)):#Cluster_setlist is list of networkx set objects.
         cluster_members = list(cluster_setlist[cluster_list]) #Converting from networkx object to list of users within a cluster
         
-        text_list = [] 
-        text_split_list = []
-        word_list = []
+        cluster_text_list = []
+        cluster_text_split_list = []
+        cluster_word_list = []
+
         
         for j in range(len(cluster_members)):
-            print(cluster_members[j])
-            print('check')
-            time.sleep(4)
+        
+            text_list = [] 
+            text_split_list = []
+            word_list = []
+        
+            #print(cluster_members[j])
+            #print('check')
+            #time.sleep(4)
             fname = 'user_timeline_' + cluster_members[j] + '.jsonl' #Converting username back to .jsonl filename
             
             with open(fname) as f: #Taking JSON file and appending just the text information for each tweet.
@@ -50,7 +59,21 @@ def frequency_analysis(cluster_setlist): #Take from network_analysis
             for k in range(len(text_split_list)): #Adds all to single list
                 for l in range(len(text_split_list[k])):
                     word_list.append(text_split_list[k][l])
-                
+                    
+            cluster_text_list.append(text_list)
+            cluster_text_split_list.append(text_split_list)
+            cluster_word_list.append(word_list)
+                    
+    print(cluster_text_list)
+    print(cluster_text_split_list)
+    print(cluster_word_list)
+    print(len(cluster_word_list))
+    print(cluster_word_list[0])
+    print(len(cluster_word_list[0]))
+    print('wut')
+    time.sleep(1000)
+        
+    '''    
             content_word_list = content_filter(word_list)
 
             fdist1 = FreqDist(content_word_list)
@@ -58,79 +81,52 @@ def frequency_analysis(cluster_setlist): #Take from network_analysis
 
             custom_words = ['RT','','-','I\'m','@'] #Second pass at removing other stopwords
             fdist1_most_common = [i for i in fdist1_most_common if i[0] not in custom_words]
-        
-            #print(len(text_list))
-            #print(word_list)
-            #print(fdist1)
-            #print(fdist1_most_common)
-            #print(type(fdist1_most_common))
-            #print('wut')
-            #time.sleep(1000)
+            frequency_unigram_stats.append(fdist1_most_common)
 
-        print('Word list length is ' + str(len(word_list)))
-        print('The cluster has ' + str(len(text_list)) + ' tweets.')
+            print('Word list length is ' + str(len(word_list)))
+            print('The cluster has ' + str(len(text_list)) + ' tweets.')
         
-        print('----------') #Visualization of most frequent words by histogram.
+            print('----------') #Visualization of most frequent words by histogram.
 
-        df = pd.DataFrame(fdist1_most_common, columns =['word', 'frequency'])
-        df.plot(kind = 'bar', x = 'word')
+            df = pd.DataFrame(fdist1_most_common, columns =['word', 'frequency'])
+            df.plot(kind = 'bar', x = 'word')
         
 
-        print('----------') # Subdiving data by bigrams
+            print('----------') # Subdiving data by bigrams
 
-        bigram_master = []
+            bigram_master = []
 
-        for bigram in range(len(text_list)):
-            word_list = []
-            word_list.append(text_list[bigram].split(" "))
+            for bigram in range(len(text_list)):
+                word_list = []
+                word_list.append(text_list[bigram].split(" "))
         
-            word_output_list = 0
+                word_output_list = 0
+            
+                for split_bigram_word in word_list: #Gets rid of nesting quirk
+                    word_output_list = split_bigram_word                
+            
+                bigrams = list(nltk.bigrams(word_output_list))
+            
+                filtered = []
+                for pairs in bigrams:
+                    if pairs[0].lower() in stopwords or pairs[1].lower() in stopwords:
+                        continue
+                    elif pairs[0].lower() in custom_words or pairs[1].lower() in custom_words:
+                        continue
+                    filtered.append(pairs)
+            
+                bigram_master.extend(filtered)
         
-            for split_bigram_word in word_list: #Gets rid of nesting quirk
-                word_output_list = split_bigram_word                
+            print(len(bigram_master))
         
-            bigrams = list(nltk.bigrams(word_output_list))
-        
-            filtered = []
-            for pairs in bigrams:
-                if pairs[0].lower() in stopwords or pairs[1].lower() in stopwords:
-                    continue
-                elif pairs[0].lower() in custom_words or pairs[1].lower() in custom_words:
-                    continue
-                filtered.append(pairs)
-        
-            bigram_master.extend(filtered)
-        
-        print(len(bigram_master))
-        
-        fdistbigram = FreqDist(bigram_master)
-        fdistbigram_common = fdistbigram.most_common(50)
+            fdistbigram = FreqDist(bigram_master)
+            fdistbigram_common = fdistbigram.most_common(50)
+            #frequency_
 
-        dfbigram = pd.DataFrame(fdistbigram_common, columns =['word', 'frequency'])
-        dfbigram.plot(kind = 'bar', x = 'word')
+            dfbigram = pd.DataFrame(fdistbigram_common, columns =['word', 'frequency'])
+            dfbigram.plot(kind = 'bar', x = 'word')
 
-        plt.show()
-
-        print('wutend')
-        time.sleep(1000)
-        
-    
-
-    
-
-    
-
-    
-
-    
-        
-    #print(fdistbigram_common)
-
-    
-
-    #print(word_list)
-
-    #plt.show()
-    
+            #plt.show()       
+    '''
 if __name__ == '__main__':
     frequency_analysis()
