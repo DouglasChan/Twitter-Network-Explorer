d9fb0718297af6719219a3205a1ca44c0e4e3f6d
DouglasChan <douglastschanto@gmail.com>
2020-06-27 14:51:31 -0400

    This is commit #2z. Got multiple geo files to be generated.

--- twitter_make_map.py
+++ twitter_make_map.py
@@ -24,27 +24,41 @@ def make_map(larger_coordinates_list,larger_text_list,larger_name_list,geo_list)
                 coordinates_right_order.append(local_list)
                 
             sample_map = folium.Map(location=[50,5], zoom_start=5)
-        
+            
             for j in range(len(larger_coordinates_list[i])):
-                name_list = larger_name_list[i]
-                text_list = larger_text_list[i]
+            
+                name_list = larger_name_list[i][j]
+                text_list = larger_text_list[i][j]
+                
                 print(name_list)
                 print(text_list)
-                #time.sleep(1)
-                
-                marker = folium.Marker(coordinates_right_order[i], popup=(name_list[j] + ':     ' + text_list[j]))
+                print(coordinates_right_order[j])
+                #time.sleep(3)
+                #print(name_list)
+                #print(text_list)
+                #print(coordinates_right_order)
+                #time.sleep(1000)
+            #time.sleep(1)
+            
+            
+                marker = folium.Marker(coordinates_right_order[j], popup=(name_list + ':     ' + text_list))
                 marker.add_to(sample_map)
-                
-                sample_map.save(geo_list[geo_counter][14:-6] + '.geo.json')
-                
-            geo_counter += 1
-                
+            
+            sample_map.save(geo_list[geo_counter][14:-6] + '.geo.json')
+        
         elif len(larger_coordinates_list[i]) == 0:
             print('It was empty!')
             print(larger_coordinates_list[i])
             time.sleep(1)
             
-            geo_counter += 1
+            #geo_counter += 1
+        
+        #for j in range(len(larger_coordinates_list[i])):
+
+            
+        geo_counter += 1
+                
+
             
             
         print('geo counter is ' + str(geo_counter))
